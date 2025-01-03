dd58bbb2dbae7b2ab47b297bfbe25e4ab34b8418
nitheeshachegondi <nitheeshaslch@gmail.com>
2025-01-03 16:38:21 +0530

    task_project

--- task_project_backend/routes/authRoutes.js
+++ task_project_backend/routes/authRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { sendOTP, verifyOTP } = require("../controllers/authController");
 const router = express.Router();
 
+// Importing the required controllers
+const {
+  sendOTP,
+  verifyOTP,
+  loginUser,
+} = require("../controllers/authController");
+
+// Routes for OTP
 router.post("/send-otp", sendOTP);
 router.post("/verify-otp", verifyOTP);
+
+// Route for Login
+router.get("/login", loginUser);
 
 module.exports = router;
--- task_project_backend/routes/layoutRoutes.js
+++ task_project_backend/routes/layoutRoutes.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const { saveLayout, getLayout } = require("../controllers/layoutController");
-const requireAuth = require("../middlewares/authMiddleware");
 const router = express.Router();
 
+// Auth Controllers
+const { loginUser } = require("../controllers/authController");
+
+// Layout Controllers
+const { saveLayout, getLayout } = require("../controllers/layoutController");
+
+// Middleware
+const requireAuth = require("../middlewares/authMiddleware");
+
+// Auth Routes
+router.get("/login", loginUser);
+
+// Layout Routes
 router.post("/save", requireAuth, saveLayout);
 router.get("/get", requireAuth, getLayout);
 
