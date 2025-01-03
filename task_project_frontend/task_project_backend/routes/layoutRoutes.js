c6a93241536c567ca1913ad9d4601440e1bf969a
nitheeshachegondi <100674658+nitheeshachegondi@users.noreply.github.com>
2025-01-03 15:44:10 +0530

    Delete task_project_frontend/task_project_backend directory

--- task_project_frontend/task_project_backend/routes/layoutRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { saveLayout, getLayout } = require("../controllers/layoutController");
-const requireAuth = require("../middlewares/authMiddleware");
-const router = express.Router();
-
-router.post("/save", requireAuth, saveLayout);
-router.get("/get", requireAuth, getLayout);
-
-module.exports = router;
